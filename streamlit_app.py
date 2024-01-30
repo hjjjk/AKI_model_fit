d64c2b6971ea81c1a0696d3dd18cb90bb3a7166a
kasser-wei <56506185+hjjjk@users.noreply.github.com>
2024-01-30 20:42:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,34 +4,29 @@ import numpy as np
 import warnings
 import shap
 import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 
 warnings.filterwarnings('ignore')
 
-model = joblib.load('aki_prediction_model.m')
+model = joblib.load('randomforest.joblib')
 
 with st.form("my_form"):
-   slider_Bodyweight = st.number_input('Body weight')
-   slider_PRI = st.number_input('Pediatric Risk of Mortality III (PRISM Ⅲ)')
-   slider_MAP = st.number_input('Minimum mean arterial pressure (MAP_min)')
-   slider_APTT = st.number_input('Activated partial thromboplastin time (APTT)')
-   num_TBil = st.number_input('Total bilirubin (TBil)')
-   num_eGFR = st.number_input('Estimated glomerular filtration rate (eGFR)')
-   slider_BUN = st.number_input('Blood urea nitrogen (BUN)')
-   num_UO = st.number_input('Urine output (UO)')
+   slider_shenzang = st.number_input('慢性肾脏病分期(shenzang)')
+   slider_age = st.number_input('年龄(age)')
+   slider_niaosuan = st.number_input('尿酸(niaosuan)')
+
 
    submitted = st.form_submit_button("Predict")
    if submitted:
-      x_test = np.array([[slider_Bodyweight, slider_PRI, slider_MAP, slider_APTT,
-         num_TBil, num_eGFR, slider_BUN, num_UO]])
+      x_test = np.array([[slider_shenzang,slider_age,slider_niaosuan]])
       explainer = shap.TreeExplainer(model)
       shap_values = explainer.shap_values(x_test)
-      temp = np.round(x_test, 2)
-      shap.force_plot(explainer.expected_value[1], shap_values[1],temp,
-         feature_names = ['Body weight','PRISM Ⅲ','MAP_min','APTT','TBil','eGFR','BUN','UO'], matplotlib=True, show=False)
-      plt.xticks(fontproperties='Times New Roman', size=15)
-      plt.yticks(fontproperties='Times New Roman', size=20)
+      shap.force_plot(explainer.expected_value[0], shap_values, x_test,
+                      feature_names=['慢性肾脏病分期(shenzang)', '年龄(age)', '尿酸(niaosuan)'], matplotlib=True, show=False)
+
       plt.tight_layout()
-      plt.savefig("19 AKI force plot.png",dpi=600)
-      pred = model.predict_proba(x_test)
-      st.markdown("#### _Based on feature values, predicted possibility of AKI is {}%_".format(round(pred[0][1], 4)*100))
-      st.image('19 AKI force plot.png')
+      plt.savefig("outcome_plot.png",dpi=600)
+      pred = model.predict(x_test)
+
+      st.markdown("#### _Based on feature values, predicted outcom is {}".format(pred[0]))
+      st.image('outcome_plot.png')
